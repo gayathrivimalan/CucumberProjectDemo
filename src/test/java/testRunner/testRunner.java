58633bd505554beeb550225d5681b7809e72201b
19712 <19712@DESKTOP-HETC9O4>
2022-11-30 00:21:29 -0800

    second commit

--- src/test/java/testRunner/testRunner.java
+++ src/test/java/testRunner/testRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features",glue ="stepDefinitions"
                  ,monochrome=true,
-plugin= {"pretty","html:target/cucumber.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+plugin= {"pretty","json:target/cucumber.json","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
          "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
         })
 public class testRunner {
